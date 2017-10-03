2454617971656c2f118539f13b69e33282aec189
Sebastian Bean <sebbean@gmail.com>
2017-10-02 23:22:05 -0700

    install data massage aahhhh

--- instashop/views.py
+++ instashop/views.py
@@ -2,22 +2,22 @@ from django.http import HttpResponse
 import json
 
 from InstagramAPI.InstagramAPI import InstagramAPI
+from instashop import data_massage
 
+def instagram_api(request):
+  api = InstagramAPI(request.GET.get('username','apicard'), request.GET.get('password','Newpassword'))
+  api.login()
+  return api
 
 def following(request):
-
-  api = InstagramAPI("apicard", "Newpassword")
-  api.login()
+  api = instagram_api(request)
   api.getSelfUsersFollowing()
   followings = api.LastJson
 
   return HttpResponse(json.dumps(followings), content_type='application/json')
 
 def liked(request):
-
-  api = InstagramAPI("apicard", "Newpassword")
-  api.login()
-
-  items = api.getTotalLikedMedia(10)
+  api = instagram_api(request)
+  items = data_massage.extract_liked_data(api.getTotalLikedMedia(int(request.GET.get('pages','1'))))
 
   return HttpResponse(json.dumps(items), content_type='application/json')

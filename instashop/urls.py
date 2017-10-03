c69febc181068d3ed52eeaed260d07ab31a21395
Sebastian Bean <sebbean@gmail.com>
2017-10-02 22:25:19 -0700

    add instagram API

--- instashop/urls.py
+++ instashop/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from instashop import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^instagram/following$', views.following),
+    url(r'^instagram/liked$', views.liked),
 ]
---
+++ instashop/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+import json
+
+from InstagramAPI import InstagramAPI
+
+
+def following(request):
+
+  api = InstagramAPI("apicard", "Newpassword")
+  api.login()
+  api.getSelfUsersFollowing()
+  followings = api.LastJson
+
+  return HttpResponse(json.dumps(followings), content_type='application/json')
+
+def liked(request):
+
+  api = InstagramAPI("apicard", "Newpassword")
+  api.login()
+
+  items = api.getTotalLikedMedia(10)
+
+  return HttpResponse(json.dumps(items), content_type='application/json')
